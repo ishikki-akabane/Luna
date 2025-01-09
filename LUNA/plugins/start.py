7090b2ad08b096b441ad462a627e9d9287342231
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-09 08:31:39 +0100

    Update start.py

--- LUNA/plugins/start.py
+++ LUNA/plugins/start.py
@@ -2,9 +2,15 @@
 
 from LUNA import TeleHook
 from telehook import Filters
-
+from LUNA.db import db
 
 @TeleHook.on_message(Filters.command('start'))
-def start_cmd(client, message):
+async def start_cmd(client, message):
+    user_id = message.from_user.id
     name = message.from_user.first_name
+    data = await db.check_user(user_id)
+    if data:
+        pass
+    else:
+        await db.add_user(user_id, name, 100, False)
     message.reply_text(f'Hello {name}')
