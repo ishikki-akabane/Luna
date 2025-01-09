966682fbb4993cd116dc7126d0b81cdbf4f7614a
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-09 07:47:31 +0100

    Update config.py

--- LUNA/config.py
+++ LUNA/config.py
@@ -4,3 +4,4 @@ class config():
     def __init__(self):
         self.BOT_TOKEN = "7774715976:AAFvr3nnh7ljMeG4QBAR2oR29dUEr4T0W4Y"
         self.WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/"
+        self.GRAMDB_URI = "https://blue-api.vercel.app/database?client=123456789&app=alpha@vaishu143.gramdb"
