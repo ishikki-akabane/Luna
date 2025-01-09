b1f72ec4a35540e41b1da943928f30a3bc66a2e7
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-09 07:44:44 +0100

    Update config.py

--- LUNA/config.py
+++ LUNA/config.py
@@ -2,5 +2,5 @@
 
 class config():
     def __init__(self):
-        self.token = "7774715976:AAFvr3nnh7ljMeG4QBAR2oR29dUEr4T0W4Y"
-        self.webhook = "https://luna-app.vercel.app"
+        self.BOT_TOKEN = "7774715976:AAFvr3nnh7ljMeG4QBAR2oR29dUEr4T0W4Y"
+        self.WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/"
