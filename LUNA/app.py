b28c494b87e79104390adc569bc6a3a791c69fbc
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-10 06:52:30 +0100

    Update app.py

--- LUNA/app.py
+++ LUNA/app.py
@@ -4,15 +4,15 @@ from LUNA import TeleHook, BOT_TOKEN
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/haha")
 def home_endpoint():
     return "Telegram Bot is running."
 
-@app.route('/webhook', methods=['POST'])
-def webhook_endpoint():
+@app.route('/', methods=['POST'])
+async def webhook_endpoint():
     try:
         update = request.get_json()
-        TeleHook.process_update(update)      
+        await TeleHook.process_update(update)      
     except Exception as e:
         print(e)
     return 'ok'
