ccb88b169aea696ec35a973f8b3a807a15024803
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-10 06:53:33 +0100

    Update config.py

--- LUNA/config.py
+++ LUNA/config.py
@@ -3,5 +3,5 @@
 class config():
     def __init__(self):
         self.BOT_TOKEN = "7774715976:AAFvr3nnh7ljMeG4QBAR2oR29dUEr4T0W4Y"
-        self.WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/"
+        self.WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/webhook"
         self.DATABASE_URI = "mongodb+srv://ishikkii:accha123@luna.tcbij.mongodb.net/?retryWrites=true&w=majority&appName=luna"
