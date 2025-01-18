58f5a7ce31a376a64448751e66f8cd7f658a860d
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-18 19:36:58 +0100

    Update config.py

--- LUNA/config.py
+++ LUNA/config.py
@@ -7,4 +7,6 @@ class config():
         self.DATABASE_URI = "mongodb+srv://ishikkii:accha123@luna.tcbij.mongodb.net/?retryWrites=true&w=majority&appName=luna"
         self.ECHOCORE_TOKEN = "12345-EchoCore-Blue-6789-353678578"
 
+        self.OWNER_ID = 1234567890
+
         self.BOT_ID = int(self.BOT_TOKEN.split(":")[0])
