843e5c0c0d5881729c62fb1f98bec763dfecf1ed
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-21 07:39:27 +0100

    Update echoplugin.py

--- LUNA/plugins/echoplugin.py
+++ LUNA/plugins/echoplugin.py
@@ -1,6 +1,6 @@
 
 from telehook import Filters
-from telehook.types import Message
+from telehook.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from LUNA import TeleHook, OWNER_ID
 from LUNA.db import db
@@ -23,5 +23,11 @@ Get 100+ Plugins for building your perfect Telegram bot
 - 100% Bug free
 - Verified by Developers
 """,
-        reply_markup
-)
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="< S H O P >", url="https://t.me/ishikki")
+                ]
+            ]
+        )
+    )
